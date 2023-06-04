5a06fba659f432389e2fd372c8298fcc9c5fba9b
Bunlong <bunlong.van@gmail.com>
2023-06-04 21:54:23 +0700

    Fix styles

--- components/demo.tsx
+++ components/demo.tsx
@@ -190,10 +190,13 @@ export default function Demo() {
     <>
       <section className={styles.section}>
         <div className={styles.row}>
-          <label>Render:</label>
+          <label>Render</label>
         </div>
         <div className={styles.row}>
-          <select onChange={(e: any) => setSelectedRenderAs(e.target.value)} className={styles.select}>
+          <select
+            className={styles.control}
+            onChange={(e: any) => setSelectedRenderAs(e.target.value)}
+          >
             <option value="canvas">Canvas</option>
             <option value="img">Image</option>
             <option value="svg">SVG</option>
@@ -202,9 +205,10 @@ export default function Demo() {
       </section>
       <section className={styles.section}>
         <div className={styles.row}>
-          <label>Text:</label>
+          <label>Text</label>
         </div>
         <input
+          className={styles.control}
           type="text"
           name="text"
           value={text}
@@ -213,7 +217,8 @@ export default function Demo() {
       </section>
       <section className={styles.section}>
         <div className={styles.row}>
-          <label>Include Options:</label>
+          <label>Include Options</label>
+          &nbsp;
           <input
             type="checkbox"
             defaultChecked
@@ -225,33 +230,44 @@ export default function Demo() {
         <legend>Options</legend>
         {selectedRenderAs === 'img' && (
           <>
-            <label>Type:</label>
-            <select
-              disabled={!includeOptions}
-              onChange={(e: any) => setSelectedType(e.target.value)}
-            >
-              <option value="image/png">image/png</option>
-              <option value="image/jpeg">image/jpeg</option>
-              <option value="image/webp">image/webp</option>
-            </select>
-            <br />
-            <label>Quality:</label>
-            <input
-              type="number"
-              min="0"
-              max="1"
-              disabled={includeOptions ? false : true}
-              value={quality}
-              step={0.1}
-              onChange={(e: any) => setQuality(e.target.value)}
-            />
+            <section className={styles.section}>
+              <div className={styles.row}>
+                <label>Type</label>
+              </div>
+              <select
+                className={styles.control}
+                disabled={!includeOptions}
+                onChange={(e: any) => setSelectedType(e.target.value)}
+              >
+                <option value="image/png">image/png</option>
+                <option value="image/jpeg">image/jpeg</option>
+                <option value="image/webp">image/webp</option>
+              </select>
+            </section>
+            <section className={styles.section}>
+              <div className={styles.row}>
+                <label>Quality</label>
+              </div>
+              <input
+                className={styles.control}
+                type="number"
+                min="0"
+                max="1"
+                disabled={includeOptions ? false : true}
+                value={quality}
+                step={0.1}
+                onChange={(e: any) => setQuality(e.target.value)}
+              />
+            </section>
           </>
         )}
-        <br />
         {selectedRenderAs !== 'svg' && (
-          <>
-            <label>Level:</label>
+          <section className={styles.section}>
+            <div className={styles.row}>
+              <label>Level</label>
+            </div>
             <select
+              className={styles.control}
               disabled={!includeOptions}
               onChange={(e: any) => setSelectedLevel(e.target.value)}
             >
@@ -260,189 +276,217 @@ export default function Demo() {
               <option value="Q">Q</option>
               <option value="H">H</option>
             </select>
-          </>
+          </section>
         )}
-        <br />
-        <label>Margin:</label>
-        <input
-          type="number"
-          min="0"
-          disabled={!includeOptions}
-          value={margin}
-          onChange={(e: any) => setMargin(e.target.value)}
-        />
-        <br />
+        <section className={styles.section}>
+          <div className={styles.row}>
+            <label>Margin</label>
+          </div>
+          <input
+            className={styles.control}
+            type="number"
+            min="0"
+            disabled={!includeOptions}
+            value={margin}
+            onChange={(e: any) => setMargin(e.target.value)}
+          />
+        </section>
         {selectedRenderAs !== 'svg' && (
-          <>
-            <label>Scale:</label>
+          <section className={styles.section}>
+            <div className={styles.row}>
+              <label>Scale</label>
+            </div>
             <input
+              className={styles.control}
               type="number"
               min="0"
               disabled={!includeOptions}
               value={scale}
               onChange={(e: any) => setScale(e.target.value)}
             />
-          </>
+          </section>
         )}
-        <br />
-        <label>Width:</label>
-        <input
-          type="number"
-          min="0"
-          disabled={!includeOptions}
-          value={width}
-          onChange={(e: any) => setWidth(e.target.value)}
-        />
-        <br />
-        <label>Dark Color:</label>
-        <div
-          style={disabled}
-          onClick={() => setDisplayDarkColorPicker(!displayDarkColorPicker)}
-        >
-          <div
-            style={{
-              width: '100%',
-              height: '29px',
-              borderRadius: '2px',
-              background: `${darkColor}`,
-            }}
+        <section className={styles.section}>
+          <div className={styles.row}>
+            <label>Width</label>
+          </div>
+          <input
+            className={styles.control}
+            type="number"
+            min="0"
+            disabled={!includeOptions}
+            value={width}
+            onChange={(e: any) => setWidth(e.target.value)}
           />
-        </div>
-        {displayDarkColorPicker && (
-          <div style={{ position: 'absolute', zIndex: '2' }}>
+        </section>
+        <section className={styles.section}>
+          <div className={styles.row}>
+            <label>Dark Color</label>
+          </div>
+          <div
+            style={disabled}
+            onClick={() => setDisplayDarkColorPicker(!displayDarkColorPicker)}
+          >
             <div
               style={{
-                position: 'fixed',
-                top: '0px',
-                right: '0px',
-                bottom: '0px',
-                left: '0px',
+                width: '100%',
+                height: '29px',
+                borderRadius: '2px',
+                background: `${darkColor}`,
               }}
-              onClick={() => setDisplayDarkColorPicker(false)}
-            />
-            <SketchPicker
-              color={darkColor}
-              onChange={(color) => setDarkColor(color.hex)}
             />
           </div>
-        )}
-        <br />
-        <label>Light Color:</label>
-        <div
-          style={disabled}
-          onClick={() => setDisplayLightColorPicker(!displayLightColorPicker)}
-        >
+          {displayDarkColorPicker && (
+            <div style={{ position: 'absolute', zIndex: '2' }}>
+              <div
+                style={{
+                  position: 'fixed',
+                  top: '0px',
+                  right: '0px',
+                  bottom: '0px',
+                  left: '0px',
+                }}
+                onClick={() => setDisplayDarkColorPicker(false)}
+              />
+              <SketchPicker
+                color={darkColor}
+                onChange={(color) => setDarkColor(color.hex)}
+              />
+            </div>
+          )}
+        </section>
+        <section className={styles.section}>
+          <div className={styles.row}>
+            <label>Light Color</label>
+          </div>
           <div
-            style={{
-              width: '100%',
-              height: '29px',
-              borderRadius: '2px',
-              backgroundColor: `${lightColor}`,
-            }}
-          />
-        </div>
-        {displayLightColorPicker && (
-          <div style={{ position: 'absolute', zIndex: '2' }}>
+            style={disabled}
+            onClick={() => setDisplayLightColorPicker(!displayLightColorPicker)}
+          >
             <div
               style={{
-                position: 'fixed',
-                top: '0px',
-                right: '0px',
-                bottom: '0px',
-                left: '0px',
+                width: '100%',
+                height: '29px',
+                borderRadius: '2px',
+                backgroundColor: `${lightColor}`,
               }}
-              onClick={() => setDisplayLightColorPicker(false)}
-            />
-            <SketchPicker
-              color={lightColor}
-              onChange={(color) => setLightColor(color.hex)}
             />
           </div>
-        )}
+          {displayLightColorPicker && (
+            <div style={{ position: 'absolute', zIndex: '2' }}>
+              <div
+                style={{
+                  position: 'fixed',
+                  top: '0px',
+                  right: '0px',
+                  bottom: '0px',
+                  left: '0px',
+                }}
+                onClick={() => setDisplayLightColorPicker(false)}
+              />
+              <SketchPicker
+                color={lightColor}
+                onChange={(color) => setLightColor(color.hex)}
+              />
+            </div>
+          )}
+        </section>
       </fieldset>
       <br />
       {selectedRenderAs === 'canvas' && (
         <>
-          <label>Include Logo:</label>
-          <input
-            type="checkbox"
-            defaultChecked
-            onChange={(e: any) => setIncludeLogo(e.target.checked)}
-          />
-          <fieldset
-            style={{
-              border: '1px solid #ccc',
-              borderRadius: 5,
-              padding: 20,
-            }}
-          >
-            <legend>Logo</legend>
-            <label>Source:</label>
-            <input
-              type="text"
-              disabled={!includeLogo}
-              value={src}
-              onChange={(e: any) => setSrc(e.target.value)}
-            />
-            <br />
-            <label>Include Options:</label>
-            <input
-              type="checkbox"
-              disabled={!includeLogo}
-              defaultChecked
-              onChange={(e: any) => setIncludeLogoOptions(e.target.checked)}
-            />
-            <br />
-            <fieldset
-              style={{
-                border: '1px solid #ccc',
-                borderRadius: 5,
-                padding: 20,
-              }}
-            >
-              <legend>Options</legend>
-              <label>Width:</label>
-              <input
-                type="number"
-                min="0"
-                disabled={!includeLogoOptions || !includeLogo}
-                value={widthLogo}
-                onChange={(e: any) => setWidthLogo(e.target.value)}
-              />
-              <br />
-              <label>Center Logo:</label>
+          <section className={styles.section}>
+            <div className={styles.row}>
+              <label>Include Logo</label>
+              &nbsp;
               <input
                 type="checkbox"
-                disabled={!includeLogoOptions || !includeLogo}
                 defaultChecked
-                onChange={(e: any) => setCenterLogo(e.target.checked)}
+                onChange={(e: any) => setIncludeLogo(e.target.checked)}
               />
-              <br />
-              <fieldset
-                style={{
-                  border: '1px solid #ccc',
-                  borderRadius: 5,
-                  padding: 20,
-                }}
-              >
+            </div>
+          </section>
+          <fieldset className={styles.fieldset}>
+            <legend>Logo</legend>
+            <section className={styles.section}>
+              <div className={styles.row}>
+                <label>Source</label>
+              </div>
+              <input
+                className={styles.control}
+                type="text"
+                disabled={!includeLogo}
+                value={src}
+                onChange={(e: any) => setSrc(e.target.value)}
+              />
+            </section>
+            <section className={styles.section}>
+              <div className={styles.row}>
+                <label>Include Options</label>
+                &nbsp;
+                <input
+                  type="checkbox"
+                  disabled={!includeLogo}
+                  defaultChecked
+                  onChange={(e: any) => setIncludeLogoOptions(e.target.checked)}
+                />
+              </div>
+            </section>
+            <fieldset className={styles.fieldset}>
+              <legend>Options</legend>
+              <section className={styles.section}>
+                <div className={styles.row}>
+                  <label>Width</label>
+                </div>
+                <input
+                  className={styles.control}
+                  type="number"
+                  min="0"
+                  disabled={!includeLogoOptions || !includeLogo}
+                  value={widthLogo}
+                  onChange={(e: any) => setWidthLogo(e.target.value)}
+                />
+              </section>
+              <section className={styles.section}>
+                <div className={styles.row}>
+                  <label>Center Logo</label>
+                  &nbsp;
+                  <input
+                    type="checkbox"
+                    disabled={!includeLogoOptions || !includeLogo}
+                    defaultChecked
+                    onChange={(e: any) => setCenterLogo(e.target.checked)}
+                  />
+                </div>
+              </section>
+              <fieldset className={styles.fieldset}>
                 <legend>X, Y Setting</legend>
-                <label>X:</label>
-                <input
-                  type="number"
-                  min="0"
-                  disabled={!includeLogoOptions || !includeLogo || !centerLogo}
-                  value={x}
-                  onChange={(e: any) => setX(e.target.value)}
-                />
-                <br />
-                <label>Y:</label>
-                <input
-                  type="number"
-                  min="0"
-                  disabled={!includeLogoOptions || !includeLogo || !centerLogo}
-                  value={y}
-                  onChange={(e: any) => setY(e.target.value)}
-                />
+                <section className={styles.section}>
+                  <div className={styles.row}>
+                    <label>X</label>
+                  </div>
+                  <input
+                    className={styles.control}
+                    type="number"
+                    min="0"
+                    disabled={!includeLogoOptions || !includeLogo || !centerLogo}
+                    value={x}
+                    onChange={(e: any) => setX(e.target.value)}
+                  />
+                </section>
+                <section className={styles.section}>
+                  <div className={styles.row}>
+                    <label>Y</label>
+                  </div>
+                  <input
+                    className={styles.control}
+                    type="number"
+                    min="0"
+                    disabled={!includeLogoOptions || !includeLogo || !centerLogo}
+                    value={y}
+                    onChange={(e: any) => setY(e.target.value)}
+                  />
+                </section>
               </fieldset>
             </fieldset>
           </fieldset>
